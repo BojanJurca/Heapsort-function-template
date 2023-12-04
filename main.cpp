3f8439e1d7bf0c36e93dc65647eaf723a817ebd2
Bojan Jurca <42709677+BojanJurca@users.noreply.github.com>
2023-12-04 08:03:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -55,72 +55,8 @@ int main () {
     std::cout << "\n\n sorted bars\n";
     for (int i = 0; i < sizeof (bars) / sizeof (bars [0]); i++)
         std::cout << "   " << bars [i] << endl;    
-    
-    
-    
-    
-    // vektorji:
-    /*
-                // https://www.geeksforgeeks.org/sorting-a-vector-in-c/
-                vector<int> v{ 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 }; 
-                // cout << v.begin() << " - " << v.end() << endl; --- to ne dela, ker sta begin in end iteratorja
-              
-                sort(v.begin(), v.end()); 
-              
-                cout << "Sorted "; 
-                for (auto x : v) 
-                    cout << x << " "; 
-                    
-                        
-                struct Interval { 
-                    int start, end; 
-                }; 
-                  
-                // Compares two intervals according to starting times. 
-                /// bool compareInterval(Interval i1, Interval i2) { return (i1.start < i2.start); } 
-                
-                vector<Interval> vi { { 6, 8 }, { 1, 9 }, { 2, 4 }, { 4, 7 } }; 
-              
-                // sort the intervals in increasing order of 
-                // start time 
-                sort(vi.begin(), vi.end(), [] (Interval i1, Interval i2) { return (i1.start < i2.start); }); 
-              
-                cout << "Intervals sorted by start time : \n"; 
-                for (auto x : vi) 
-                    cout << "[" << x.start << ", " << x.end << "] "; 
 
-
-    // locale: https://en.cppreference.com/w/cpp/locale/collate
-
-    std::locale::global(std::locale("en_US.utf8"));
-    std::wcout.imbue(std::locale(""));
-    std::vector<std::wstring> vl{
-        L"ar", L"zebra", L"\u00f6grupp", L"Zebra",
-        L"\u00e4ngel",L"\u00e5r", L"f\u00f6rnamn"
-    };
-
-    std::wcout << "Default locale collation order: ";
-    std::sort(vl.begin(), vl.end());
-    for (auto s : vl)
-        std::wcout << s << ' ';
-    std::wcout << '\n';
-
-    std::wcout << "English locale collation order: ";
-    std::sort(vl.begin(), vl.end(), std::locale("en_US.UTF-8"));
-    for (auto s : vl)
-        std::wcout << s << ' ';
-    std::wcout << '\n';
-
-    // std::wcout << "Swedish locale collation order: ";
-    // std::sort(vl.begin(), vl.end(), std::locale("sv_SE.UTF-8"));
-    // for (auto s : vl)
-    //     std::wcout << s << ' ';
-    // std::wcout << '\n';
-
-    // https://en.cppreference.com/w/cpp/locale/collate/compare
-
-    */
-
+   
     return 0;
 }
 
